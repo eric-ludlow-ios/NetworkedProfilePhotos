5ef4a434f7d71aa6b36876af419d4df2a47e9e1c
Eric Ludlow <esludlow@gmail.com>
2018-04-04 00:21:18 -0600

    sets nav bar color scheme

---
+++ NetworkedProfilePhotos/UIColor+NPP.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+NPP.swift
+//  NetworkedProfilePhotos
+//
+//  Created by Eric Ludlow on 4/3/18.
+//  Copyright © 2018 EricLudlow. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static var headerBlue: UIColor {
+        return UIColor(red: 14.0/255, green: 107.0/255, blue: 148.0/255, alpha: 1.0)
+    }
+}
